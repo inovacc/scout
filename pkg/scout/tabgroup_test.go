504c0c5f5945d34fca8146100ce71ad6fece557a
Dyam Marcano <dyam.marcano@gmail.com>
2026-03-01 13:22:59 -0300

    feat: add Do, DoAll, DoParallel, Broadcast to TabGroup

--- pkg/scout/tabgroup_test.go
+++ pkg/scout/tabgroup_test.go
@@ -61,6 +61,117 @@ func TestTabGroupOptions(t *testing.T) {
 	}
 }
 
+func TestTabGroupDo(t *testing.T) {
+	srv := newTestServer()
+	defer srv.Close()
+	b := newTestBrowser(t)
+
+	tg, err := b.NewTabGroup(2)
+	if err != nil {
+		t.Fatalf("NewTabGroup: %v", err)
+	}
+	defer func() { _ = tg.Close() }()
+
+	err = tg.Do(0, func(p *Page) error {
+		return p.Navigate(srv.URL)
+	})
+	if err != nil {
+		t.Fatalf("Do(0): %v", err)
+	}
+	title, err := tg.Tab(0).Title()
+	if err != nil {
+		t.Fatalf("Title: %v", err)
+	}
+	if title == "" {
+		t.Fatal("expected non-empty title")
+	}
+}
+
+func TestTabGroupDoAll(t *testing.T) {
+	srv := newTestServer()
+	defer srv.Close()
+	b := newTestBrowser(t)
+
+	tg, err := b.NewTabGroup(3)
+	if err != nil {
+		t.Fatalf("NewTabGroup: %v", err)
+	}
+	defer func() { _ = tg.Close() }()
+
+	err = tg.DoAll(func(i int, p *Page) error {
+		return p.Navigate(srv.URL)
+	})
+	if err != nil {
+		t.Fatalf("DoAll: %v", err)
+	}
+	for i := 0; i < 3; i++ {
+		title, err := tg.Tab(i).Title()
+		if err != nil {
+			t.Fatalf("Tab(%d) Title: %v", i, err)
+		}
+		if title == "" {
+			t.Fatalf("Tab(%d) expected non-empty title", i)
+		}
+	}
+}
+
+func TestTabGroupDoParallel(t *testing.T) {
+	srv := newTestServer()
+	defer srv.Close()
+	b := newTestBrowser(t)
+
+	tg, err := b.NewTabGroup(3)
+	if err != nil {
+		t.Fatalf("NewTabGroup: %v", err)
+	}
+	defer func() { _ = tg.Close() }()
+
+	errs := tg.DoParallel(func(i int, p *Page) error {
+		return p.Navigate(srv.URL)
+	})
+	for i, e := range errs {
+		if e != nil {
+			t.Fatalf("DoParallel tab %d: %v", i, e)
+		}
+	}
+}
+
+func TestTabGroupBroadcast(t *testing.T) {
+	srv := newTestServer()
+	defer srv.Close()
+	b := newTestBrowser(t)
+
+	tg, err := b.NewTabGroup(3)
+	if err != nil {
+		t.Fatalf("NewTabGroup: %v", err)
+	}
+	defer func() { _ = tg.Close() }()
+
+	errs := tg.Broadcast(func(p *Page) error {
+		return p.Navigate(srv.URL)
+	})
+	for i, e := range errs {
+		if e != nil {
+			t.Fatalf("Broadcast tab %d: %v", i, e)
+		}
+	}
+}
+
+func TestTabGroupDoOutOfRange(t *testing.T) {
+	b := newTestBrowser(t)
+
+	tg, err := b.NewTabGroup(1)
+	if err != nil {
+		t.Fatalf("NewTabGroup: %v", err)
+	}
+	defer func() { _ = tg.Close() }()
+
+	err = tg.Do(5, func(p *Page) error { return nil })
+	if err == nil {
+		t.Fatal("expected error for out-of-range index, got nil")
+	}
+}
+
 func TestTabGroupCloseIdempotent(t *testing.T) {
 	b := newTestBrowser(t)
 
